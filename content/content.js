2d2578df0dbcfe3770bc22418e86b70dd12a4011
maazahmadkhan1 <76567679+maazahmadkhan1@users.noreply.github.com>
2022-04-08 23:36:59 +0500

    08/04/2022
    check inputs on keypress

--- content/content.js
+++ content/content.js
@@ -1,6 +1,8 @@
+let have_an_account = false;
+let keywords =['name','login','email','signin','pay','card','cvv','password','checkout','paypal','cvc','order','account','signup','username'];
 chrome.runtime.onMessage.addListener(function(req, sender, sendResponse){
 	if (req.msg == "giveDomain") {
-		var url = window.location.hostname.replace("www.","");
+		var url = window.location.hostname;
 		sendResponse(url);
 	} 
 	if (req.msg == "inject") {
@@ -31,28 +33,75 @@ chrome.runtime.onMessage.addListener(function(req, sender, sendResponse){
 });
 
 $(document).ready(function () {
+
   $(document).on("click", "button", function () {
     console.log($(this).text());
-    if ($(this).text().toLowerCase() == "login" && $(this).text().toLowerCase() == "log in" && $(this).text().toLowerCase() == "sign in") {
+    if ($(this).text().toLowerCase() == "login" || $(this).text().toLowerCase() == "log in" || $(this).text().toLowerCase() == "sign in") {
       var elems = document.querySelectorAll("*"),
       res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email');  
       console.log(res ? 'found!' : 'not found');
-    } else if ($(this).text().toLowerCase() == "register" && $(this).text().toLowerCase() == "sign up") {
+    } else if ($(this).text().toLowerCase() == "register" || $(this).text().toLowerCase() == "sign up") {
       var elems = document.querySelectorAll("*"),
-      res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email');  
+      res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email');
+      if(res){
+        saveDomain();
+      } 
       console.log(res ? 'found!' : 'not found');
     }
   });
-  $(document).on("click", "input[type='submit']", function () {
+
+  $(document).on("click", "input[type='submit'],input[type='button']", function () {
     console.log($(this).text());
-    if ($(this).text().toLowerCase() == "login" && $(this).text().toLowerCase() == "log in" && $(this).text().toLowerCase() == "sign in") {
+    if ($(this).text().toLowerCase() == "login" || $(this).text().toLowerCase() == "log in" || $(this).text().toLowerCase() == "sign in") {
       var elems = document.querySelectorAll("*"),
       res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email');  
       console.log(res ? 'found!' : 'not found');
-    } else if ($(this).text().toLowerCase() == "register" && $(this).text().toLowerCase() == "sign up") {
+    } else if ($(this).text().toLowerCase() == "register" || $(this).text().toLowerCase() == "sign up") {
       var elems = document.querySelectorAll("*"),
-      res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email');  
+      res = Array.from(elems).find(v => v.textContent.toLowerCase() == 'name' || v.textContent.toLowerCase() == 'email'); 
+      if(res){
+        saveDomain();
+      }
       console.log(res ? 'found!' : 'not found');
     }
   });
+
+  document.querySelectorAll('input').forEach((input,index)=>{
+      input.addEventListener('change',function(){
+        if(input.value.length>2){
+         checkInputs(Object.values(input.attributes));
+        }
+      })
+  })
+
 });
+
+
+function saveDomain(){
+
+  let domain = window.location.hostname;
+
+  chrome.storage.local.get('domains',function({domains=[]}){
+      if(domains.indexOf(domain)==-1){
+        domains.push(domain);
+        chrome.storage.local.set({domains});
+      }
+  })
+
+}
+
+function checkInputs(attributes){
+    console.log(attributes);
+    if(attributes.length>0){
+      attributes.forEach(function(attr,ind){
+        if(have_an_account==false){
+          keywords.forEach(function(keyword){
+              if(attr.value.toLowerCase().indexOf(keyword.toLowerCase())>-1){
+                saveDomain();
+              }
+          })
+        }
+      });
+  }
+    
+  }
--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	chrome.tabs.sendMessage(tabs[0].id, { msg: "giveDomain" }, function (response) {
-		$(".url").text(response);
+		let url = response;
+		if(response.indexOf('www.')>-1){
+			url = response.replace("www.","");
+		}
+		$(".url").text(url);
+		chrome.storage.local.get('domains',({domains})=>{
+			console.log(domains);
+			if(domains.indexOf(response)>-1){
+
+				$('#have_an_account').attr('checked',true);
+			}
+		});
 	});
 });
 
@@ -9,3 +20,4 @@ $("#close").click(function () {
     	chrome.tabs.sendMessage(tabs[0].id, { msg: "remove" });
   	});
 });
+
